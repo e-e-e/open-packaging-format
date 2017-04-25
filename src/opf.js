c81b555f690c4c9f838eba028f392348d5a30025
e-e-e <e-e-e@users.noreply.github.com>
2017-04-25 21:18:21 +1000

    add extra get methods

--- src/opf.js
+++ src/opf.js
@@ -20,36 +20,68 @@ export class OPF {
     return this.getField('dc:title');
   }
 
-  set title(s) {
-    this.obj['dc:title'] = s;
+  get allTitles() {
+    return this.getList('dc:title');
   }
 
   get authors() {
     return this.getList('dc:creator');
   }
 
+  get contributors() {
+    return this.getList('dc:contributors');
+  }
+
   get description() {
     return this.getField('dc:description');
+  }
+
+  get publishers() {
+    return this.getList('dc:publishers');
   }
 
   get subjects() {
     return this.getList('dc:subject');
   }
 
+  get format() {
+    return this.getField('dc:format');
+  }
+
+  get type() {
+    return this.getField('dc:type');
+  }
+
+  get date() {
+    // TO DO:
+    // opf-event data should be fetched as well.
+    // detect and convert to date object (YYYY[-MM[-DD]])
+    return this.getField('dc:date');
+  }
+
+  get source() {
+    return this.getField('dc:source');
+  }
+
+  get coverage() {
+    return this.getField('dc:coverage');
+  }
+
+  get rights() {
+    return this.getField('dc:rights');
+  }
+
   getList(name, id = '_') {
-    if (name in this.obj) {
-      if (Array.isArray(this.obj[name])) {
-        return this.obj[name].map(c => c[id]);
-      }
+    if (Array.isArray(this.obj[name])) {
+      return this.obj[name].map(c => c[id]);
     }
     return undefined;
   }
 
-  getField(name, idx = 0) {
-    if (name in this.obj) {
-      if (Array.isArray(this.obj[name]) && this.obj[name].length > idx) {
-        return this.obj[name][idx];
-      }
+  getField(name, index = 0) {
+    const field = this.obj[name];
+    if (field && Array.isArray(field) && field.length > index) {
+      return this.obj[name][index];
     }
     return undefined;
   }
